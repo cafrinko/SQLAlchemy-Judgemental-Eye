329c6e3419ca90671bb3392ff714d597aa27cdf2
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-27 18:02:35 -0700

    Created routes to users and made login/logout sessions a thing

--- server.py
+++ server.py
@@ -21,6 +21,8 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
+
+    print session
     return render_template("homepage.html")
 
 @app.route('/users')
@@ -28,7 +30,17 @@ def user_list():
     """Show list of users."""
 
     users = User.query.all()
+
     return render_template("user_list.html", users=users)
+
+@app.route('/user_info/<user_id>')
+def show_user_info(user_id):
+    """Displays individual user age, zipcode, and list of movies rated (and scores for each rating)"""
+
+    user = User.query.get(user_id)
+
+
+    return render_template("user_info.html", user=user)
 
 @app.route('/register', methods=["GET"])
 def register_form():
@@ -43,6 +55,8 @@ def register_process():
     email = request.form.get('useremail')
     password = request.form.get('userpassword')
 
+    #.first() preferred over .one() because .first() will return None if no value in db
+    #.one() will error out if no value in db
     user = User.query.filter_by(email=email).first()
     if not user:
         new_user = User(email=email, password=password)
@@ -67,31 +81,36 @@ def login_process():
     email = request.form.get('useremail')
     password = request.form.get('userpassword')
 
+    # gives access to entire user object
     user = User.query.filter_by(email=email, password=password).first()
+    # db session lasts as long as query does
 
     if user:
-        session[email] = password
+        # can access user_id value now that we've queried for user and have access to whole object
+        session['user_id'] = user.user_id
         flash("You are now logged in.")
     else:
         flash("Your username does not match your password. Please try again with a different username password combination.")
 
-    return redirect('/')
+    return redirect('/user_info')
 
 @app.route('/logout', methods=["GET"])
 def logout_form():
-    """Displays logout button"""
+    """Displays logout button and allows user to log out"""
 
-    return render_template('logout_form.html')
+    # return render_template('logout_form.html')
 
-@app.route('/logout', methods=["POST"])
-def logout_process():
-    """Logs user out"""
+# @app.route('/logout', methods=["POST"])
+# def logout_process():
+#     """Logs user out"""
     
-    logout = request.form.get('logout')
+#     logout = request.form.get('logout')
     session.clear()
     flash('You are now logged out')
 
     return redirect('/')
+
+
 
 
 
